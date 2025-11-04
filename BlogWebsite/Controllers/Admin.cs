8dde8ec81a7886d69ea86017679c3ef04d72a196
Emre Yaşıru <emreyasiru@gmail.com>
2025-11-04 22:08:25 +0300

    Add project files.

---
+++ BlogWebsite/Controllers/Admin.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogWebsite.Controllers
+{
+    public class Admin : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
