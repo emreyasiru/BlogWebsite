9b9b8e8c44967c6f6bf66b3e2a776d734a20f614
Emre Yaşıru <emreyasiru@gmail.com>
2025-11-05 00:14:10 +0300

    up

--- BlogWebsite/wwwroot/AnaTemplate/js/main.js
+++ BlogWebsite/wwwroot/AnaTemplate/js/main.js
@@ -43,30 +43,27 @@ $(function() {
     navbarFixed();
 
 
-  if ($('.blog-slider').length) {
-    $('.blog-slider').owlCarousel({
-        loop: true,
-        margin: 30,
-        items: 1,
-        nav: true,
-        autoplay: 2500,
-        smartSpeed: 1500,
-        dots: false,
-        responsiveClass: true,
-        navText : ["<div class='blog-slider__leftArrow'><img src='img/home/left-arrow.png'></div>","<div class='blog-slider__rightArrow'><img src='img/home/right-arrow.png'></div>"],
-        responsive:{
-          0:{
-              items:1
-          },
-          600:{
-              items:2
-          },
-          1000:{
-              items:3
-          }
-      }
-    })
-  }
+    if ($('.blog-slider').length) {
+        $('.blog-slider').owlCarousel({
+            loop: true,
+            margin: 30,
+            items: 1,
+            nav: true,
+            autoplay: 2500,
+            smartSpeed: 1500,
+            dots: false,
+            responsiveClass: true,
+            navText: [
+                "<div class='blog-slider__leftArrow'><img src='/AnaTemplate/img/home/left-arrow.png'></div>",
+                "<div class='blog-slider__rightArrow'><img src='/AnaTemplate/img/home/right-arrow.png'></div>"
+            ],
+            responsive: {
+                0: { items: 1 },
+                600: { items: 2 },
+                1000: { items: 3 }
+            }
+        })
+    }
 
   //------- mailchimp --------//  
 	function mailChimp() {
