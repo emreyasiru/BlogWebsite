280a8753597c7871828b8d4774a111122e9b10d7
Emre Yaşıru <emreyasiru@gmail.com>
2025-11-06 02:53:46 +0300

    up

--- BlogWebsite/Program.cs
+++ BlogWebsite/Program.cs
@@ -36,7 +36,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Admin}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
